9992ea0ac1fe3e6950c1c83f832908989c2c88aa
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-04-17 18:27:10 +0200

    update web app angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
+//librerias de módulos
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Componentes
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
+//Decorador de módulos
 @NgModule({
+  //Componentes
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
