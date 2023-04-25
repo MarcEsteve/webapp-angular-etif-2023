ccf05da55c2190b483a25c72fcc28ff3907de0a3
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-04-25 15:58:39 +0200

    añado también la ruta de terminos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
+import { TerminosComponent } from './pages/terminos/terminos.component';
 
 const app_routes: Routes = [
   { path: '', component: PortfolioComponent }, //Ruta principal
   { path: 'about', component: AboutComponent },
   { path: 'item', component: ItemComponent },
+  { path: 'terminos' , component: TerminosComponent},
   { path: '**', pathMatch: 'full', redirectTo: '' },
   //Totes les altres rutes que no existeixin a les anteriors
 ];
