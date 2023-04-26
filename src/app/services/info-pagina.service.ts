f0558c20d6cfff772278c9c750e0e8662c2a0b00
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-04-26 17:03:41 +0200

    Interfaz para darle tipos a los datos del JSON

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InfoPaginaService {
-  info: any;
+  info: InfoPagina = {};
 
   constructor(private http: HttpClient) {
     // console.log("Servicio de infoPagina listo")
@@ -13,7 +14,7 @@ export class InfoPaginaService {
     // Leer el archivo JSON
     this.http
     .get('assets/data/data-pagina.json')
-    .subscribe((resp: any) => {
+    .subscribe((resp: InfoPagina) => {
       this.info = resp; // provar resp. I veurem les propietats JSON
       console.log(resp);
     });
