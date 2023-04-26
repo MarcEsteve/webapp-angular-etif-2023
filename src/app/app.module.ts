55efd26774eebc9ef85a2698a8fa91c7d1422ade
Marc Esteve Garcia <41911219+MarcEsteve@users.noreply.github.com>
2023-04-26 16:33:11 +0200

    HTTP client, info guarda JSON y http la forma de acceder a los datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 //Componentes
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { ItemComponent } from './pages/item/item.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InfoPaginaService {
+  info: any;
 
-  constructor() {
-    console.log("Servicio de infoPagina listo")
+  constructor(private http: HttpClient) {
+    // console.log("Servicio de infoPagina listo")
+
+    // Leer el archivo JSON
+    this.http
+    .get('assets/data/data-pagina.json')
+    .subscribe((resp: any) => {
+      this.info = resp; // provar resp. I veurem les propietats JSON
+      console.log(resp);
+    });
   }
 }
